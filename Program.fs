31fd4e63824d9677253d349cbbf9d32680ce1a30
xeno <xeno@lv427.space>
2022-12-01 06:11:55 +0100

    day 1 solutions

--- Program.fs
+++ Program.fs
@@ -1,4 +1,27 @@
-﻿[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+﻿open System.IO
+
+let input = File.ReadAllLines "/Users/xeno/projects/aoc/day1/input.txt" |> Array.toList
+
+input |> List.map (printfn "%A")
+
+let rec sumcals acc lines =
+    match lines with
+    | ""::rest -> acc :: (sumcals 0 rest)
+    | i::rest -> sumcals ((i |> int) + acc) rest
+    | [] -> [acc]
+
+let sums = sumcals 0 input
+
+let m = sums |> List.max 
+printfn "AAA"
+
+sums |> List.map (printfn "%A")
+
+printfn "%A" m
+
+let ordered = sums |> List.sort |> List.rev
+
+let a::b::c::r = ordered
+let z = a + b + c 
+
+printfn "%A" z 
